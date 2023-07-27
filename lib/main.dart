e293d20d20a75ec19ab6a5c5dd9dd2cf5f7748fe
harshj-1703 <harshj6680@gmail.com>
2023-07-27 14:58:29 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,216 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MyWidget();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyWidget> createState() => _MyWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  void initState() {
+    myprocess1();
+    super.initState();
+  }
 
-  void _incrementCounter() {
+  var name = '';
+  var dob = '';
+  var phone = '';
+  var city = '';
+  var state = '';
+  var country = '';
+  var image = 'https://randomuser.me/api/portraits/men/41.jpg';
+  bool isLoading = false;
+
+  void myprocess1() async {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      isLoading = true;
+    });
+    var url = Uri.parse('https://randomuser.me/api/');
+    var response = await http.get(url);
+    var details = jsonDecode(response.body)['results'][0];
+    if (details['gender'] != 'female') {
+      myprocess1();
+    } else {
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${details}');
+      setState(() {
+        name = details['name']['first'] + ' ' + details['name']['last'];
+        dob = details['dob']['age'].toString();
+        phone = details['phone'];
+        city = details['location']['city'];
+        state = details['location']['state'];
+        country = details['location']['country'];
+        image = details['picture']['large'];
+      });
+      changeLoading();
+    }
+  }
+
+  void changeLoading() {
+    setState(() {
+      isLoading = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    if (isLoading) {
+      return MaterialApp(
+          title:
+              'RandomUsers', //app name shown from package name and for main title this is.
+          theme: ThemeData(primarySwatch: Colors.cyan), //theme of app
+          home: Scaffold(
+            //scaffold
+            appBar: AppBar(
+              //appbar for show scaffold title
+              title: const Text(
+                'RandomUsers',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 17, 2, 177),
+                  letterSpacing: 1.5,
+                  fontSize: 21,
+                  fontWeight: FontWeight.w900,
+                  fontStyle: FontStyle.italic,
+                  fontFamily: 'Times New Roman',
+                ),
+              ), //title of scaffold
+              centerTitle: true,
+              toolbarHeight: 50,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            body: Center(
+              child: CircularProgressIndicator(),
             ),
-          ],
+          ));
+    } else {
+      return MaterialApp(
+        title:
+            'RandomUsers', //app name shown from package name and for main title this is.
+        theme: ThemeData(primarySwatch: Colors.cyan), //theme of app
+        home: Scaffold(
+          //scaffold
+          appBar: AppBar(
+            //appbar for show scaffold title
+            title: const Text(
+              'RandomUsers',
+              style: TextStyle(
+                color: Color.fromARGB(255, 17, 2, 177),
+                letterSpacing: 1.5,
+                fontSize: 21,
+                fontWeight: FontWeight.w900,
+                fontStyle: FontStyle.italic,
+                fontFamily: 'Times New Roman',
+              ),
+            ), //title of scaffold
+            centerTitle: true,
+            toolbarHeight: 50,
+          ),
+          body: Container(
+            width: double.infinity,
+            height: double.infinity,
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  padding:
+                      EdgeInsets.all(16), // Add padding to the whole container
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200], // Optional background color
+                    borderRadius: BorderRadius.circular(
+                        10), // Add rounded corners to the container
+                  ),
+                  child: Column(
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 100,
+                            // The background image will be null if the image is not loaded yet.
+                            backgroundImage:
+                                isLoading ? null : NetworkImage(image),
+                          ),
+                          if (isLoading) CircularProgressIndicator(),
+                        ],
+                      ),
+                      SizedBox(
+                          height:
+                              10), // Add some space between the avatar and the text
+                      Text(
+                        'Name: ${name}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'Age: ${dob}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'Phone No.: ${phone}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'City: ${city}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'State: ${state}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text(
+                        'Country: ${country}',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      SizedBox(
+                          height:
+                              20), // Add some space between the text and the button
+                      ElevatedButton(
+                        onPressed: () {
+                          myprocess1();
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 20,
+                              vertical: 10), // Adjust button padding
+                          primary: Color.fromARGB(
+                              255, 11, 3, 129), // Set button color
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                5), // Add rounded corners to the button
+                          ),
+                        ),
+                        child: Text(
+                          "Find Another One",
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+      );
+    }
   }
 }
